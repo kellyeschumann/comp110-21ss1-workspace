09e0c724324dadd65ccd26c650926af14b2d64a5
Kelly Schumann <kellys16@live.unc.edu>
2021-05-20 23:26:25 -0400

    progress on exercise 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,20 @@
-"""DESCRIPTION OF PROGRAM HERE"""
+"""When given a name, this hype machine sends you messages to motivate you"""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730314660"
 
-# Your solution starts here...
+name: str = input("What is your name? ")
+print("You entered: ")
+print(name)
+
+hypeMessageOne = "You are so smart "
+y = "!"
+print(hypeMessageOne + name + y)
+
+hypeMessageTwo = "You can do this "
+hypeMessageThree = " I know you can!"
+print(hypeMessageTwo + name  + hypeMessageThree)
+
+hypeMessageFour = " have a great night!"
+print(name + hypeMessageFour)
+
+
--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -1,5 +1,35 @@
-"""DESCRIPTION OF PROGRAM HERE"""
+"""This takes two numbers input from user and completes four mathematical equations"""
 
-__author__: str = "YOUR PID HERE"
+from typing import Sized, SupportsIndex
 
-# Your solution starts here...
+
+__author__: str = "730314660"
+
+l = "Left-hand side"
+r = "Right-hand side"
+
+numberOne: str = input("What is the first number? ")
+print("You entered: ")
+print(numberOne)
+
+numberTwo: str = input("What is the second number? ")
+print("You entered: ")
+print(numberTwo)
+
+print(l + ": " + numberOne)
+print(r + ": " + numberTwo)
+
+integerOne = int(numberOne)
+integerTwo = int(numberTwo)
+
+resultOne = integerOne ** integerTwo
+resultTwo = integerOne / integerTwo
+resultThree = integerOne // integerTwo
+resultFour = integerOne % integerTwo
+
+print(numberOne + " ** " + numberTwo + " is " + str(resultOne))
+print(numberOne + " / " + numberTwo + " is " + str(resultTwo))
+print(numberOne + " // " + numberTwo + " is " + str(resultThree))
+print(numberOne + " % " + numberTwo + " is " + str(resultFour))
+
+
